afcac46a50dc087f6abc87106b31e26749e0e89b
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-04-30 13:04:53 -0500

    completed simple_functions.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,31 @@
+"use strict"
+
+
+// function to display my name, favorite movie, and favorite band
+function favoriteThings() {
+
+    console.log("My name is Will");
+    console.log("My favorite movie is Into the Spider-Verse");
+    console.log("I dont really have a favorite band, i just listen to whatever comes up");
+
+}
+
+
+
+
+
+
+
+function whyImHere() {
+    console.log("The reason why im here is because of a family member recommended me to go through this program, and so far I am liking it so far. Hopefully I can follow through this program.")
+}
+
+function favoritePlace(){
+    console.log("One of my favorite place is probably my home. There's no place like home.")
+}
+
+// calling/invoking our functions
+
+favoriteThings();
+whyImHere();
+favoritePlace();
