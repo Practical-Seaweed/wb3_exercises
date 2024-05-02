9ffbe3e4a0caaf68bf593b518687f10debb6866c
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-02 14:20:32 -0500

    added replace_strings.js

---
+++ strings/replace_strings.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+function replaceDtoA (){
+let message = "Our corporate offices are located in Dallas";
+let result = message.replace("Dallas", "Austin");
+
+console.log(result)
+}
+
+replaceDtoA();
