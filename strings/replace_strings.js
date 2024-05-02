2d39165ded6d3e91dadef19e8beff21a28ab0c58
Wilfredo Contreras <wilfredojrcontreras@gmail.com>
2024-05-02 14:57:44 -0500

    added replace_strings.js

--- strings/replace_strings.js
+++ strings/replace_strings.js
@@ -1,11 +1,23 @@
 "use strict"
 
+// this is regulaar replacement
+function replaceDtoA() {
+    let message = "Our corporate offices are located in Dallas";
+    let result = message.replace("Dallas", "Austin");
 
-function replaceDtoA (){
-let message = "Our corporate offices are located in Dallas";
-let result = message.replace("Dallas", "Austin");
+    console.log(result)
+}
 
-console.log(result)
+replaceDtoA();
+
+
+
+// this is case-insensitive replacement.
+function replaceDtoA() {
+    let message = "Our corporate offices are located in Dallas";
+    let result = message.replace(/DALLAS/i, "Austin");
+
+    console.log(result)
 }
 
 replaceDtoA();
